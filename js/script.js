a6670f8c0123611ff74a74cdaa2ab66fc5a09330
rc2pc2 <petriccarcc@gmail.com>
2023-11-23 10:25:57 +0100

    minesweeper livecoding

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ startButtonEl.addEventListener('click', function(){
 function generateNewGame(wrapperElement, levelSelector){
     wrapperElement.innerHTML = '';
 
+
+    let userScore = 0;
+    let isGameOver = false;
+
     const level = parseInt(levelSelector.value);
     let cellsNo;
 
@@ -34,26 +38,69 @@ function generateNewGame(wrapperElement, levelSelector){
 
     let cellsPerRow = Math.sqrt(cellsNo);
 
+    const bombs = [];
+    while( bombs.length < 16){
+        let randomNumber = getRandomNumber(1, cellsNo)
+        if ( !bombs.includes(randomNumber) ){
+            bombs.push(randomNumber);
+        }
+    }
+
+    console.log(bombs);
+
     for (let i = 1 ; i <= cellsNo ; i++){
         const currentSquare = getNewSquare();
         const squareContent = i;
-
-        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
 
         const cellSize = `calc(100% / ${cellsPerRow})`;
         currentSquare.style.width = cellSize;
         currentSquare.style.height= cellSize;
 
-        if ( squareContent % 2 === 0){
-            currentSquare.classList.add('bg-blue');
-        } else {
+        if ( bombs.includes(squareContent) ){
             currentSquare.classList.add('bg-red');
         }
 
         currentSquare.addEventListener('click', function(){
-            currentSquare.classList.toggle('clicked');
-            console.log(squareContent);
-        });
+            if (!isGameOver){
+                currentSquare.classList.add('clicked');
+
+                if ( !bombs.includes(squareContent) ){
+                    const bombChecker = [
+                        squareContent - 1,
+                        squareContent + 1,
+                        squareContent - cellsPerRow -1,
+                        squareContent - cellsPerRow,
+                        squareContent - cellsPerRow + 1,
+                        squareContent + cellsPerRow - 1,
+                        squareContent + cellsPerRow ,
+                        squareContent + cellsPerRow + 1
+                    ];
+
+                    let closeBombs = 0;
+
+                    for (let index = 0; index < bombChecker.length; index++) {
+                        if (bombs.includes(bombChecker[index])){
+                            closeBombs++;
+                        }
+                    }
+
+                    currentSquare.innerHTML += `<span> ${closeBombs} </span>`;
+
+
+                    currentSquare.classList.add('bg-blue');
+                    console.log(++userScore);
+                    updateCurrentScore(`Your score is ${userScore}`);
+                } else {
+                    isGameOver = true;
+                    currentSquare.innerHTML = `<span> <i class="fa-solid fa-bomb fa-beat"></i> </span>`;
+                    console.log('BOOOM!');
+                    updateCurrentScore(`BOOM! Game over: your high score is ${userScore}`);
+                    // ? tutte le altre bombe, scopritevi.
+                    showBombCells();
+                    //
+                }
+            }
+        }, { once: true });
 
         wrapperElement.appendChild(currentSquare);
     }
@@ -80,4 +127,19 @@ function getNewSquare(){
  */
 function getRandomNumber(minNumber, maxNumber){
     return Math.floor( Math.random() *(maxNumber - minNumber + 1) + minNumber);
+}
+
+function updateCurrentScore(scoreToUpdate){
+    const scoreboardEl = document.getElementById('scoreboard');
+    scoreboardEl.innerText = scoreToUpdate;
+}
+
+function showBombCells(){
+    const bombElements = document.querySelectorAll('article.bg-red');
+    for (let index = 0; index < bombElements.length; index++) {
+        const bombElement = bombElements[index];
+        bombElement.classList.add('clicked');
+        bombElement.innerHTML = `<span> <i class="fa-solid fa-bomb fa-beat"></i> </span>`;
+
+    }
 }
